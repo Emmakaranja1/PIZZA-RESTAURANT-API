89e0afe735ebe0f3283b28761afdde0c837343fd
Emmakaranja1 <karanjaemmak@gmail.com>
2025-06-23 11:49:43 +0300

    add __init__.py files to models, controllers for package initialization

---
+++ server/models/__init__.py
@@ -0,0 +1,6 @@
+from server.models.pizza import Pizza
+from server.models.restaurant import Restaurant
+from server.models.restaurant_pizza import RestaurantPizza
+
+
+__all__ = ['Restaurant', 'Pizza', 'RestaurantPizza']
